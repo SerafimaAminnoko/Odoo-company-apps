42937cbf15732dc99fc0449d2231c12d279832e2
SerafimaAminnoko <132136596+SerafimaAminnoko@users.noreply.github.com>
2024-11-25 15:12:39 +0200

    created model for simple tasks, simple kanban view, and tree view for tasks

--- company_task_manager/models/models.py
+++ company_task_manager/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
 #class company_task_manager(models.Model):
@@ -15,5 +15,37 @@
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record in self:
-#             record.value2 = float(record.value) / 100
+#             record.value2
+#             float(record.value) / 100
 
+
+class GlobalTasks(models.Model):
+    _name = 'company_task_manager.global_tasks'
+    _description = 'company_task_manager.global_tasks'
+
+    name = fields.Char()
+    description = fields.Text()
+    data_add = fields.Datetime()
+    data_closed = fields.Datetime()
+
+
+class Employee(models.Model):
+    _name = 'employees'
+    _description = 'employees'
+
+    name = fields.Char()
+    data_add = fields.Datetime(default=fields.Datetime.now)
+
+
+class Tasks(models.Model):
+    _name = 'tasks'
+    _description = 'tasks'
+
+    title = fields.Char()
+    description = fields.Text()
+    time_create = fields.Datetime(default=fields.Datetime.now)
+    employee_id = fields.Many2one("employees", string="Partner")
+    status = fields.Selection(
+        [('pending', 'Pending'), ('in process', 'In Process'),
+         ('completed', 'Completed'), ('canceled', 'Canceled')]
+    )
